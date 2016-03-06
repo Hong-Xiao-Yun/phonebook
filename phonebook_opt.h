6f3606fc4bdf1786fc472c6ac1ee15c7ee72015e
Hong-Xiao-Yun <Hong-Xiao-Yun@users.noreply.github.com>
2016-03-06 20:12:28 +0800

    Added files via upload

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -6,8 +6,8 @@
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
 // #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+typedef struct detail
+{
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,6 +17,10 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+}info;
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    struct  info *detail;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
